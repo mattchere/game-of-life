0739a55d9fc18aa051ececfb60ca5587ca7bddbb
infinitepure <matt.j.cheah@gmail.com>
2017-09-16 00:49:57 +1000

    Create basic components

---
+++ src/components/Cell.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Cell extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Cell;
---
+++ src/components/SettingsBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class SettingsBar extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default SettingsBar;
