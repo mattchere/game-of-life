c0012d3b9000f5fe7e71801734ad5852b60cc587
Matt Cheah <matt.j.cheah@gmail.com>
2019-02-09 19:15:59 +1100

    Updates

---
+++ precache-manifest.64e94584e58347882acbd7c8e624b273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "2098670f019dc80ee493",
+    "url": "/game-of-life/static/js/main.2098670f.chunk.js"
+  },
+  {
+    "revision": "5ce6da4a102466ca4efb",
+    "url": "/game-of-life/static/js/1.5ce6da4a.chunk.js"
+  },
+  {
+    "revision": "2098670f019dc80ee493",
+    "url": "/game-of-life/static/css/main.ade1331b.chunk.css"
+  },
+  {
+    "revision": "526d151376b8f469515a3067c054dcad",
+    "url": "/game-of-life/index.html"
+  }
+];
